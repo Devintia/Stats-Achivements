e94eb139767abd688f2bd0bf09ce245303a657ca
Tom Sacher <tom.sacher@mcggehrden.de>
2016-06-18 22:06:11 +0200

    [+] Tried aroud with DB

--- src/main/java/net/devintia/stats/Stats.java
+++ src/main/java/net/devintia/stats/Stats.java
@@ -1,11 +1,16 @@
 package net.devintia.stats;
 
+import lombok.Getter;
 import net.devintia.stats.StatManagers.StatType;
 import net.devintia.stats.listeners.PSIE;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import net.devintia.db.DbHandler;
 
 public final class Stats extends JavaPlugin implements Listener {
+
+    @Getter
+    private DbHandler dbHandler;
 
     @Override
     public void onEnable() {
