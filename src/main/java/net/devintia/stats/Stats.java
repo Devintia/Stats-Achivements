066e624a82a940a2f458c80987f51bdd33c2cac0
Tom Sacher <tom.sacher@mcggehrden.de>
2016-06-18 22:49:19 +0200

    [+] Tried aroud with DB

--- src/main/java/net/devintia/stats/Stats.java
+++ src/main/java/net/devintia/stats/Stats.java
@@ -1,11 +1,16 @@
 package net.devintia.stats;
 
 import lombok.Getter;
-import net.devintia.stats.StatManagers.StatType;
+import net.devintia.db.DbHandlerFactory;
+import net.devintia.db.DbType;
 import net.devintia.stats.listeners.PSIE;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import net.devintia.db.DbHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public final class Stats extends JavaPlugin implements Listener {
 
@@ -17,7 +22,51 @@ public final class Stats extends JavaPlugin implements Listener {
         PSIE psie = new PSIE();
         getServer().getPluginManager().registerEvents( psie, this );
 
+        dbHandler = DbHandlerFactory.getNewDbHandler( DbType.YAML ); //YAML for test purposes
+        dbHandler.init();
+
+        dbHandler.save( "key", new DBTest( "test", 1 ) );
+        DBTest test = (DBTest) dbHandler.get( "key" );
+        if ( test.testString.equals( "test" ) && test.testInt == 1 ) {
+            System.out.println( "sucess!" );
+        } else {
+            System.out.println( "fail!" );
+        }
+        dbHandler.update( "key", new DBTest( "test2", 2 ) );
+        test = (DBTest) dbHandler.get( "key" );
+        if ( test.testString.equals( "test2" ) && test.testInt == 2 ) {
+            System.out.println( "sucess!" );
+        } else {
+            System.out.println( "fail!" );
+        }
+
     }
+
+    class DBTest implements ConfigurationSerializable{
+
+        String testString;
+        int testInt;
+
+        public DBTest( String s, int i ) {
+            testString = s;
+            testInt = i;
+        }
+
+        public DBTest( Map<String, Object> data ) {
+            testInt = (int) data.get( "testInt" );
+            testString = (String) data.get( "testString" );
+        }
+
+        @Override
+        public Map<String, Object> serialize() {
+            Map<String, Object> map = new HashMap<>();
+            map.put( "testInt", testInt );
+            map.put( "testString", testString );
+            return map;
+        }
+    }
+
+
 
     @Override
     public void onDisable() {
---
+++ src/main/java/net/devintia/stats/database/FetchFromDatabase.java
@@ -0,0 +1,14 @@
+package net.devintia.stats.database;
+
+import org.bukkit.entity.Player;
+
+/**
+ * Created by tom on 18.06.16.
+ */
+public class FetchFromDatabase {
+
+    public static void fetch(Player p){
+
+    }
+
+}
